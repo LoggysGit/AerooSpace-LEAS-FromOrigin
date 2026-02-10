55085c32a9b5b989a3fd562585e2886f7553ca83
LoggysGit <albertmetsler3008@gmail.com>
2026-02-10 23:52:50 +0500

    Developing

--- _ai_tests.py
+++ _ai_tests.py
@@ -1,3 +1,4 @@
+import asyncio
 # = Modules =
 import modules.parser as parser     # Data Parser
 import modules.controller as model  # AI Model Controller
@@ -8,14 +9,14 @@ import modules.registrator as reg   # API Key Manager
 MODEL_PATH = "assets/model/Qwen2.5-7B-Instruct-Q4_K_M.gguf"
 
 # === Objects & Variables ===
-data_fetcher = parser.DataControlManager()
 ai = model.AIModel(MODEL_PATH)
+data_fetcher = parser.DataControlManager()
 
 # === Test ===
 data_fetcher.setInput("custom", 45.9646, 63.3052, "2026-02-13T12:00:00.000000Z", "UTC+5")
-#data_fetcher.fetchAllData()
+asyncio.run(data_fetcher.fetchAllData())
 full_prompt = data_fetcher.getFullPrompt()
 print(full_prompt)
 
 ai.setRole("")
-ai.analyze(full_prompt)
+#ai.analyze(full_prompt)
